13e158754b24762904b313d03ee272c837623cbf
watermark <bryan@theclays.us>
2016-02-25 20:55:17 -0500

    Add CustomerGroup resource
    Add CustomerGroup resource

---
+++ src/Bigcommerce/Api/Resources/CustomerGroup.php
@@ -0,0 +1,16 @@
+<?php
+namespace Bigcommerce\Api\Resources;
+
+use Bigcommerce\Api\Resource;
+use Bigcommerce\Api\Client;
+
+class CustomerGroup extends Resource
+{
+	protected $ignoreOnCreate = array(
+	  'id'
+  );
+  
+	protected $ignoreOnUpdate = array(
+	  'id'
+	);
+}
